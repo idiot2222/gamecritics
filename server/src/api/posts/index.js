70f9e79d7d5836773de10bba61f23a35ba2dd5ab
yadon079 <zeidgame@gmail.com>
2021-06-16 12:39:42 +0900

    feature : 서버 제작 중간저장
    <description>
    + 서버 제작 중간 저장
    <issue-number>

---
+++ server/src/api/posts/index.js
@@ -0,0 +1,12 @@
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
+
+const posts = new Router();
+
+posts.get('/', postsCtrl.list);
+posts.post('/', postsCtrl.write);
+posts.get('/:id', postsCtrl.read);
+posts.delete('/:id', postsCtrl.remove);
+posts.patch('/:id', postsCtrl.update);
+
+export default posts;
---
+++ server/src/api/posts/posts.ctrl.js
@@ -0,0 +1,40 @@
+import Post from '../../models/post';
+
+
+export const write = async ctx => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+
+
+export const list = async ctx => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+
+
+export const read = ctx => {};
+
+
+
+export const remove = ctx => {};
+
+
+
+export const update = ctx => {};
