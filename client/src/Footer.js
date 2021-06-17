4486dc1569a1b50ed5403d4523adee23ccbc48e8
GraceNam <zeidgame@gmail.com>
2021-06-18 00:40:41 +0900

    docs : 푸터 내용 추가
    <description>
    + 푸터 내용 추가
    <issue-number>

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
   const items = ['Gamecritics', '김민정', '박현근', '최현진', '한선우'];
 
   return (
-    <div className="botbody">
+    <div className="botbody container-fluid">
       <div className="footer">
         <div>
           <div
@@ -69,13 +69,84 @@ const Footer = () => {
         <div className="topBtn" onClick={() => window.scrollTo(0, 0)}>
           <i className="fas fa-chevron-up"></i>
         </div>
+        <div className="textBox">
+          <div className="subtitle text-color mobile-border-color">
+            <span>사이트 맵</span>
+          </div>
+          <ul className="footerLinker first">
+            <li>
+              <a href="https://localhost:3000/login">
+                로그인
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/register">
+                회원가입
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/category">
+                게임
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/notice">
+                공지사항
+              </a>
+            </li>
+          </ul>
+          <ul className="footerLinker second">
+            <li>
+              <a href="https://localhost:3000/login">
+                팬 아트 정책
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/register">
+                뉴스룸
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/category">
+                커뮤니티 규칙
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/notice">
+                사용자 경험 조사
+              </a>
+            </li>
+          </ul>
+          <ul className="footerLinker third">
+            <li>
+              <a href="https://localhost:3000/login">
+                고객센터
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/register">
+                EULA
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/category">
+                문의사항
+              </a>
+            </li>
+            <li>
+              <a href="https://localhost:3000/notice">
+                자주하는 질문
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       <hr className="mobile-border-color"></hr>
       <div className="row copyright">
         <div className="copyright-paragraph">
           <span className="text-color-nonactive">
             © 2021, 3조. All Rights Reserved. GC, Gamecritics, Gamecritics
-            로고의 모든 저작권은 Epic Games, Inc.에 있으며, 이는 3조 및 관계자에
+            로고의 모든 저작권은 Gamecritics, Inc.에 있으며, 이는 3조 및 관계자에
             모두 해당됩니다. 기타 브랜드 또는 제품 이름은 해당 소유자의
             상표입니다.
           </span>
