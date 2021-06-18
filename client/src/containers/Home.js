a80977655de262655a6fa8beaefc85d0090bdb16
santos019 <santos019@naver.com>
2021-06-18 09:23:32 +0900

    리스트 길이수정

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,9 +1,9 @@
-import React from "react";
-import "./Home.scss";
-import SimpleSlider from "../components/MainImage";
-import HomeCenter1 from "../components/home/HomeCenter1";
-import HomeCenter2 from "../components/home/HomeCenter2";
-import HomeCenter3 from "../components/home/HomeCenter3";
+import React from 'react';
+import './Home.scss';
+import SimpleSlider from '../components/MainImage';
+import HomeCenter1 from '../components/home/HomeCenter1';
+import HomeCenter2 from '../components/home/HomeCenter2';
+import HomeCenter3 from '../components/home/HomeCenter3';
 const Home = () => {
   return (
     <div className="homeindex">
@@ -13,13 +13,12 @@ const Home = () => {
       <div className="homecenter1">
         <HomeCenter1 />
       </div>
-      <div>
-        <HomeCenter3/>
+      <div className="homecenter3">
+        <HomeCenter3 />
       </div>
       <div className="homecenter2">
         <HomeCenter2 />
       </div>
-     
     </div>
   );
 };
